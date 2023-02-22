555d3b57383219af8cb3dc8c636aa4a23d2c4ef0
Esteban Barrios <esteban.barrios@schuettflix.com>
2023-02-22 13:30:27 +0100

    Added missing )

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const spawnSync = require('child_process').spawnSync;
 const path = require("path");
 
-const proc = spawnSync('bash', [path.join(__dirname, 'entrypoint.sh')], {stdio: 'inherit'};
+const proc = spawnSync('bash', [path.join(__dirname, 'entrypoint.sh')], {stdio: 'inherit'});
 process.exit(proc.status);
