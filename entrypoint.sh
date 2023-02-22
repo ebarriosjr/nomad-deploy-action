74e2f3589b021abe5d9ddd3f698d5a07c107b48c
Esteban Barrios <esteban.barrios@schuettflix.com>
2023-02-22 11:49:32 +0100

    Update comment on entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - && \
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" && \
 sudo apt-get update && sudo apt-get install nomad
 
-//VARS should be a list of var_name=value
+# VARS should be a list of var_name=value
 VARS=""
 for i in $VARIABLES
 do
