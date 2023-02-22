c133434c6a17de8b7f06230090aa33b115fd7527
Esteban Barrios <esteban.barrios@schuettflix.com>
2023-02-22 14:02:43 +0100

    Added check to see if nomad is installed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - && \
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" && \
-sudo apt-get -qq update && sudo apt-get -qq install nomad
+if ! command -v nomad &> /dev/null
+then
+  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - && \
+  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" && \
+  sudo apt-get update && sudo apt-get install nomad
+fi
 
 # VARS should be a list of var_name=value
 VARS=""
